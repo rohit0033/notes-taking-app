36c130ee8a692c640d00d23ef8e687cddd44839d
rohit0033 <rohusharma2003@gmail.com>
2025-02-03 18:44:36 +0530

    api

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => res.send('API is running...'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
